b770f0851471fcad55e2e53ee747d7a06ddabd1a
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-04-30 21:35:18 +0100

    :page_facing_up: Consumindo dados estaticos do Todo

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image } from "react-native";
 
 import { Container } from "./styles";
 
-const Details = () => {
+const Details = ({ navigation, route }) => {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    console.log("route => ", route.params.todo.item);
+    setTodo(route.params.todo.item);
+  }, []);
+
   return (
     <Container>
-      <h1> Details Page </h1>
+      <h1> {todo.title} </h1>
+      <Text> {todo.desc} </Text>
+      <Image source={{ uri: todo.imageUrl }} />
+      <Text> {todo.createdAt} </Text>
+      <Text> {todo.doAt} </Text>
     </Container>
   );
 };
