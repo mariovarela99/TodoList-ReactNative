08bd67b13e5fcd465d6b49e085abce3bb730574f
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-04-30 21:31:07 +0100

    :rocket: Criada Estrutura Basica

--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { View, Button, Text, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Button,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 import electron from "../../assets/images/electronjs.jfif";
 import js from "../../assets/images/js.png";
-import { Container, ImageStyles } from "./styles";
+import { Container } from "./styles";
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  tinyLogo: {
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  TodoImage: {
     width: 50,
     height: 50,
   },
@@ -25,20 +44,65 @@ const styles = StyleSheet.create({
 });
 
 const TodoList = ({ navigation }) => {
+  const DATA = [
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Iniciar Com React Native",
+      desc: " Uma Description Basica do Todo.! ",
+      imageUrl: "../../assets/images/js.png",
+      createdAt: "22/07/2021",
+      doAt: "25/07/2021",
+    },
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "Iniciar Com ElectronJS",
+      desc: " Uma Description Basica do Todo.! ",
+      imageUrl: "../../assets/images/js.png",
+      createdAt: "22/07/2021",
+      doAt: "02/08/2021",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Fazer a tarefa de TLP",
+      desc: " Uma Description Basica do Todo.! ",
+      imageUrl: "../../assets/images/js.png",
+      createdAt: "22/07/2021",
+      doAt: "02/08/2021",
+    },
+  ];
+
+  const Item = ({ item }) => (
+    <View style={styles.item}>
+      <Image
+        styles={styles.TodoImage}
+        resizeMode="contain"
+        source={require("../../assets/images/js.png")}
+      />
+      <Text style={styles.title}>{item.title}</Text>
+      <Button
+        styles={styles.button}
+        title={item.title}
+        onPress={() =>
+          navigation.navigate("Details", {
+            todo: { item },
+          })
+        }
+      />
+    </View>
+  );
+
+  const renderItem = ({ item }) => {
+    return <Item item={item} />;
+  };
+
   return (
-    <Container>
-      <h1> Todo List </h1>
-      <ul>
-        <li>
-          <Image styles={styles.tinyLogo} source={electron} />
-          <Button
-            styles={styles.button}
-            title="Iniciar com React Native"
-            onPress={() => navigation.navigate("Details")}
-          />
-        </li>
-      </ul>
-    </Container>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 };
 
